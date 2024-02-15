a5564071a801cdf9c061c4b42074a9ad78dcfe43
VirtshyDiaz <144281458+VirtshyDiaz@users.noreply.github.com>
2024-02-15 10:31:59 -0600

    inicio d eproyecto

---
+++ src/Chatbot_with_switch.java
@@ -0,0 +1,19 @@
+
+import java.util.Scanner;
+
+
+public class Chatbot_with_switch {
+    public static void main(String[] args) {
+        Scanner scan = new scan(System.in);
+
+        // Mostrar opciones al usuario
+        System.out.println("¿Qué deseas hacer?");
+        System.out.println("1. Libro de recetas con presupuesto");
+        System.out.println("2. Cursos de cocina");
+        System.out.println("3. Planeador de menú");
+        System.out.println("4. Lista de supermercado");
+        System.out.println("5. Recetas específicas");
+        System.out.println("6. Salir del menú");
+    }
+    
+}
